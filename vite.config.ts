f352ccc3d32453f1d4911bee688cfa869322b4d1
Abdullah <abdullah@kodifly.com>
2025-09-15 20:55:22 +0300

    fix : vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 
-export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-    proxy: {
-      "/proxy": {
-        target: "https://api-testing.early.app/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (p) => p.replace(/^\/proxy/, ""),
-        headers: {
-          Origin: "https://product-testing.early.app",
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [mkcert()],
+    server: {
+      // Proxy from https://127.0.0.1:<port>/proxy/... to https://api-testing.early.app/...
+      proxy: {
+        "/proxy": {
+          target: "https://api-testing.early.app/",
+          rewrite: (path) => path.replace(/^\/proxy/, ""),
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on("proxyReq", (proxyReq, _req, _res) => {
+              proxyReq.setHeader("Origin", "https://product-testing.early.app");
+            });
+          },
         },
       },
     },
-  },
+  };
 });
